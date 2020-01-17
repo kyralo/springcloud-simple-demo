f748c4dc43a997cc159749a7a17aee7f27722abb
kyralo <kyralo721@gmail.com>
2020-01-17 22:21:06 +0800

    add more

--- shop_web_gateway/src/main/java/online/kyralo/shop/web/gateway/ShopWebGatewayApplication.java
+++ shop_web_gateway/src/main/java/online/kyralo/shop/web/gateway/ShopWebGatewayApplication.java
@@ -1,25 +1,25 @@
-package online.kyralo.shop.web.zuul;
+package online.kyralo.shop.web.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * \* Created with Intellij IDEA.
  * \* @author: WangChen
- * \* Date: 2020-01-12
- * \* Time: 0:56
+ * \* Date: 2020-01-13
+ * \* Time: 14:57
  * \* Year: 2020
  * \
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableZuulProxy
-public class ShopWebZuulApplication {
+public class ShopWebGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopWebZuulApplication.class, args);
+        SpringApplication.run(ShopWebGatewayApplication.class, args);
     }
 }
